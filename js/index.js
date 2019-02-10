a7dfb95b841feb95075b92f967129b91944a1394
Jasraj Singh Bedi <jasraj.singh@corowave.com>
2019-02-10 05:39:41 -0500

    integrated solace

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 $("#doc").click(function(){
 	window.location = "doc.html";
 });
+$("#sub").click(function(){
+	if($("#email").val() == "admin"  && $("#password").val() == "admin"){
+		window.location = "try-me-publisher/index.html";
+	}
+});
+
 $("#pat").click(function(){
-	alert(3);
+	window.location = "try-me-subscriber/index.html";
 });
