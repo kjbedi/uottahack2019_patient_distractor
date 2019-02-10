3dbd5a5a8f0dd8b8f766fe8895e9ad0ca27ad7d3
Jasraj Singh Bedi <jasraj.singh@corowave.com>
2019-02-10 07:13:34 -0500

    fixed conflict

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 $("#doc").click(function(){
 	window.location = "doc.html";
 });
-<<<<<<< HEAD
 
-$("#pat").click(function(){
-	window.location = "patient.html";
-});
-
-$("#patbutton").click(function(){
-	window.location = "./webvr/index.html";
-});
-
-=======
 $("#sub").click(function(){
 	if($("#email").val() == "admin"  && $("#password").val() == "admin"){
 		window.location = "try-me-publisher/index.html";
@@ -21,4 +11,4 @@ $("#sub").click(function(){
 $("#pat").click(function(){
 	window.location = "try-me-subscriber/index.html";
 });
->>>>>>> a7dfb95b841feb95075b92f967129b91944a1394
+
